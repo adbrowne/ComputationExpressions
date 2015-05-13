e39811a8e4def3c0c310527444f86d4adc340781
Andrew Browne <brownie@brownie.com.au>
2015-05-13 22:47:35 +1000

    Fill in github details

--- build.fsx
+++ build.fsx
@@ -9,10 +9,10 @@
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted
-let gitOwner = "myGitUser"
+let gitOwner = "adbrowne"
 let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
-let gitProjectName = "MyProject"
+let gitProjectName = "ComputationExpressions"
 
 open FsReveal
 open Fake
